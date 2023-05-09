ee7823c4fffc84170c8b8e1af971d9b61aaebc44
nlf <quitlahok@gmail.com>
2023-05-09 10:58:05 -0700

    chore: add comment to create-cert script

--- scripts/create-cert.sh
+++ scripts/create-cert.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+# if you start getting errors about an expired certificate, run this script and commit the changes
 
 openssl req -nodes -new -x509 -keyout test/fixtures/fake-key.pem -out test/fixtures/fake-cert.pem -subj '/CN=US'
