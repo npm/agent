81b9cd659826671e883bb15d004d386b210b6f71
Michael Garvin <gar+gh@danger.computer>
2026-04-16 19:58:14 -0700

    fix: clean up socksv5 lib
    Get the code working again, and clean out parts we don't need for our tests

--- test/fixtures/socksv5/constants.js
+++ test/fixtures/socksv5/constants.js
@@ -1,14 +1,14 @@
 exports.CMD = {
   CONNECT: 0x01,
   BIND: 0x02,
-  UDP: 0x03
-};
+  UDP: 0x03,
+}
 
 exports.ATYP = {
   IPv4: 0x01,
   NAME: 0x03,
-  IPv6: 0x04
-};
+  IPv6: 0x04,
+}
 
 exports.REP = {
   SUCCESS: 0x00,
@@ -19,5 +19,5 @@ exports.REP = {
   CONNREFUSED: 0x05,
   TTLEXPIRED: 0x06,
   CMDUNSUPP: 0x07,
-  ATYPUNSUPP: 0x08
-};
+  ATYPUNSUPP: 0x08,
+}
--- test/fixtures/socksv5/index.js
+++ test/fixtures/socksv5/index.js
@@ -1,15 +1,8 @@
-var fs = require('fs'),
-    path = require('path');
+const { createServer } = require('./server.js')
+const { NoneAuthHandlers, UserPasswordAuthHandlers } = require('./auth.js')
 
-['server', 'client', 'Agents'].forEach(function(f) {
-  var exp = require(__dirname + '/lib/' + f),
-      keys = Object.keys(exp);
-  for (var i = 0, len = keys.length; i < len; ++i)
-    exports[keys[i]] = exp[keys[i]];
-});
-
-exports.auth = {};
-
-fs.readdirSync(__dirname + '/lib/auth').forEach(function(f) {
-  exports.auth[path.basename(f, '.js')] = require(__dirname + '/lib/auth/' + f);
-});
+exports.createServer = createServer
+exports.auth = {
+  None: NoneAuthHandlers,
+  UserPassword: UserPasswordAuthHandlers,
+}
